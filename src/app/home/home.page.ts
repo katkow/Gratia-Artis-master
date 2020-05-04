256507f095cfec64ecce6bb969ebe7571670c55c
katkow <45760814+katkow@users.noreply.github.com>
2020-05-05 00:29:03 +0200

    Initial commit

---
+++ src/app/home/home.page.ts
@@ -0,0 +1,26 @@
+import { Component } from '@angular/core';
+import { MenuController } from '@ionic/angular';
+
+
+
+@Component({
+  selector: 'app-home',
+  templateUrl: 'home.page.html',
+  styleUrls: ['home.page.scss'],
+})
+export class HomePage {
+
+  constructor(private menu: MenuController) {}
+
+  openMenu() {
+    this.menu.enable(true, 'menu');
+    this.menu.open('custom');
+  }
+
+}
+
+
+
+
+
+
