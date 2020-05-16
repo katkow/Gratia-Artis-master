184340b5ace278bfff01f2a9767c89b7fb5c02a3
katkow <45760814+katkow@users.noreply.github.com>
2020-05-16 02:20:44 +0200

    16.05 update
    -upload pliku rejestrowany w bazie danych firebase
    -zakladka profil: po zalogowaniu wyswietla wszystkie poprzednio wrzucone pliki (patrz: login jako user: tester [hasło: haslo!@#])

--- src/app/firebase.ts
+++ src/app/firebase.ts
@@ -1,13 +1,12 @@
 var firebaseConfig = {
-    apiKey: "AIzaSyBAr1RXsAU3C66S3anbGMjWiATz40dLI5E",
-    authDomain: "gratiaartis-447b4.firebaseapp.com",
-    databaseURL: "https://gratiaartis-447b4.firebaseio.com",
-    projectId: "gratiaartis-447b4",
-    storageBucket: "gratiaartis-447b4.appspot.com",
-    messagingSenderId: "1077561376578",
-    appId: "1:1077561376578:web:3078ef8acc5c9c7c8692db",
-    measurementId: "G-78C1ZFL94J"
-  }
+  apiKey: "AIzaSyC2bXtGfcTtoLzGWxiCiw5oeLcAsvxlHtk",
+  authDomain: "gratia-artis.firebaseapp.com",
+  databaseURL: "https://gratia-artis.firebaseio.com",
+  projectId: "gratia-artis",
+  storageBucket: "gratia-artis.appspot.com",
+  messagingSenderId: "305011697433",
+  appId: "1:305011697433:web:e3da8b31cc00746e33c788"
+};
 
   export default firebaseConfig
   
--- src/app/registration/registration.page.ts
+++ src/app/registration/registration.page.ts
@@ -1,8 +1,11 @@
 import { Component, OnInit } from '@angular/core';
 import { AngularFireAuth } from '@angular/fire/auth';
-import { auth } from 'firebase/app'
-import { AlertController } from '@ionic/angular'
-import { Router } from '@angular/router'
+import { auth } from 'firebase/app';
+import { AlertController, MenuController } from '@ionic/angular';
+import { Router } from '@angular/router';
+import { AngularFirestore } from '@angular/fire/firestore'
+import { UserService } from '../user.service';
+
 @Component({
   selector: 'app-registration',
   templateUrl: './registration.page.html',
@@ -16,38 +19,59 @@ export class RegistrationPage implements OnInit {
 
   constructor(
     public afAuth: AngularFireAuth,
-    public alert: AlertController,
-    public router: Router
+    public alertController: AlertController,
+    public router: Router,
+    public menu: MenuController,
+    public afstore: AngularFirestore,
+    public user: UserService
     ) { }
 
   ngOnInit() {
   }
 
+  async presentAlert(title: string, content: string) {
+    const alert = await this.alertController.create({
+      header: title,
+      message: content,
+      buttons: ["Ok"]
+    })
+
+    await alert.present()
+  }
+
   async register() {
     const { username, password, cpassword } = this
     if(password !== cpassword) {
-      this.showAlert("Błąd", "Hasła nie są zgodne!")
+      this.presentAlert("Błąd", "Hasła nie są zgodne!")
       return console.error("Hasła nie są zgodne")
     }
     try{
     const res = await this.afAuth.createUserWithEmailAndPassword(username + '@gmail.com', password)
     console.log(res)
-    this.showAlert("Rejestracja przebiegła pomyślnie", "Witamy na stronie!")
+    //this.presentAlert("Rejestracja przebiegła pomyślnie", "Witamy na stronie!")
+    //this.router.navigate(['/tabs'])
+
+      this.afstore.doc(`users/${res.user.uid}`).set({
+        username
+      })
+
+    this.user.setUser({
+      username,
+      uid: res.user.uid
+    })
+    this.presentAlert('Gratulacje!', 'Rejestracja przebiegła pomyślnie')
     this.router.navigate(['/tabs'])
+
+
     } catch(error) {
       console.dir(error)
-      this.showAlert("Error", error.message)
+      this.presentAlert("Error", error.message)
     }
   }
 
-  async showAlert(header: string, message: string) {
-    const alert = await this.alert.create({
-      header,
-      message,
-      buttons: ["Ok"]
-    })
-
-    await alert.present()
+  
+  openCart() {
+    this.menu.open('cart');
   }
 
 }
