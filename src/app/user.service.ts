1f9fa0bcdcbe61dd0f3823516dac98d2b998c024
katkow <45760814+katkow@users.noreply.github.com>
2020-05-16 21:59:20 +0200

    Revert "Revert "16.05 update""
    This reverts commit 60d1d9a4a4312ba5cd4e6327c0ef39b1d0a56c50.

---
+++ src/app/user.service.ts
@@ -0,0 +1,22 @@
+import { Injectable } from '@angular/core'
+import { AngularFireAuth } from '@angular/fire/auth'
+
+interface user {
+    username: string,
+    uid: string
+}
+@Injectable()
+export class UserService {
+    private user: user
+
+    constructor(){
+
+    }
+    setUser(user: user) {
+        this.user = user
+    }
+
+    getUID() {
+        return this.user.uid
+    }
+}
