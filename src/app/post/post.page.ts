14269b102a836c5ff42acced7d1a3c343eddd32c
katkow <45760814+katkow@users.noreply.github.com>
2020-05-18 00:27:55 +0200

    likes
    Like is being saved to database now.  Post page remembers users' decision - if the img was liked, the heart icon stays filled out

--- src/app/post/post.page.ts
+++ src/app/post/post.page.ts
@@ -1,6 +1,8 @@
 import { Component, OnInit } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
-import { AngularFirestore } from '@angular/fire/firestore';
+import { AngularFirestore, AngularFirestoreDocument } from '@angular/fire/firestore';
+import { UserService } from '../services/user.service';
+import { firestore } from 'firebase/app'
 
 @Component({
   selector: 'app-post',
@@ -11,21 +13,45 @@ export class PostPage implements OnInit {
 
   postID: string
   post
+  postReference: AngularFirestoreDocument
+  sub
 
   heartType: string = "heart-outline" 
   //default - puste serce
 
-  constructor(private route: ActivatedRoute, private afs: AngularFirestore) { 
+  constructor(
+    private route: ActivatedRoute, 
+    private afs: AngularFirestore,
+    private user: UserService) { 
     
   }
 
   ngOnInit() {
     this.postID = this.route.snapshot.paramMap.get('id')
-    this.post = this.afs.doc(`posts/${this.postID}`).valueChanges()
+    this.postReference = this.afs.doc(`posts/${this.postID}`)
+    this.sub = this.postReference.valueChanges().subscribe(val => {
+      this.post = val
+      this.heartType = val.likes.includes(this.user.getUID()) ? 'heart' : 'heart-outline'
+    })
   }
 
+  ngOnDestroy() {
+    this.sub.unsubscribe()
+  }
+
+
+
   toggleHeart() {
-    this.heartType = this.heartType == "heart" ? "heart-outline" : "heart"
+
+    if(this.heartType == 'heart-outline') {
+      this.postReference.update({
+        likes: firestore.FieldValue.arrayUnion(this.user.getUID())
+      })
+    } else {
+      this.postReference.update({
+        likes: firestore.FieldValue.arrayRemove(this.user.getUID())
+      })
+    }
   } 
 
 }
